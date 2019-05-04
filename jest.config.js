8eb734b8bc166c62de99c8c7ac15f3328d98622c
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-05-04 17:08:06 +0200

    Fix Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: [
+    "node_modules/(?!react-native|@shoutem|react-clone-referenced-element|native-base-shoutem-theme|react-native-camera)"
+  ]
 }
