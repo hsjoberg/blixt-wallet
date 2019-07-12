cbaeb8fbe270544695dba8359b7451d7c695e895
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-07-12 22:59:28 +0200

    Update to react-native 0.60

--- android/app/src/main/java/com/blixtwallet/LndProcessIntentService.java
+++ android/app/src/main/java/com/blixtwallet/LndProcessIntentService.java
@@ -2,21 +2,12 @@ package com.blixtwallet;
 
 import android.app.ActivityManager;
 import android.app.IntentService;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Process;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
+import androidx.annotation.Nullable;
 
-import java.util.concurrent.TimeUnit;
+import android.util.Log;
 
 public class LndProcessIntentService extends IntentService {
     private static final String TAG = "LndProcessIntentService";
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import { AppRegistry, YellowBox } from "react-native";
+import App from "./src/App";
+import {name as appName} from "./app.json";
+
+YellowBox.ignoreWarnings(["Warning: component"]); // Workaround until native-base fixes their old
+                                                  // componentWillMount etc code
 
 AppRegistry.registerComponent(appName, () => App);
