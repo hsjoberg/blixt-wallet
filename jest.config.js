8b70bec590c15be8337ecd150917b46381c4adda
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-08-04 14:07:28 +0200

    Moving tests from /__tests__ to /tests

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,13 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!react-native|@shoutem|react-clone-referenced-element|native-base-shoutem-theme|react-native-camera|@react-navigation|react-navigation-tabs|@react-native-community\/async-storage)"
   ],
-  "setupFiles": [
+  setupFiles: [
     "./jestSetup.js",
     "jest-date-mock"
   ],
-  "moduleNameMapper": {
+  moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js",
     "\\.(css|less)$": "<rootDir>/assetsTransformer.js"
-  }
+  },
+  testMatch: ['**/tests/**/*.[jt]s?(x)'],
 }
