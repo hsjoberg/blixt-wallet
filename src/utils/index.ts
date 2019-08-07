f603f71902e10853d5ab5ee7a1c47729e2705bc4
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-08-07 19:47:44 +0200

    Fixing ISO date being wrong

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,5 @@
+import { format } from "date-fns";
+
 export const capitalize = (word: string) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
+export const formatISO = (date: Date) => format(date, "yyyy-MM-dd HH:mm");
