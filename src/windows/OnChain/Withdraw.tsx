ce20d46e3db687be4189d6935571fd5debfcbcf9
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-08-12 00:02:16 +0200

    Bug fixes and improvements

--- src/windows/OnChain/Withdraw.tsx
+++ src/windows/OnChain/Withdraw.tsx
@@ -13,19 +13,24 @@ export interface IOpenChannelProps {
 }
 export default ({ navigation }: IOpenChannelProps) => {
   const sendCoins = useStoreActions((actions) => actions.onChain.sendCoins);
+  const sendCoinsAll = useStoreActions((actions) => actions.onChain.sendCoinsAll);
   const getBalance = useStoreActions((actions) => actions.onChain.getBalance);
   const [address, setAddress] = useState("");
   const [sat, setSat] = useState("");
   const [sending, setSending] = useState(false);
   const [camera, setCamera] = useState(false);
+  const [withdrawAll, setWithdrawAll] = useState(false);
 
   const onWithdrawClick = async () => {
     try {
       setSending(true);
-      const result = await sendCoins({
-        address,
-        sat: Number.parseInt(sat, 10),
-      });
+      let result;
+      if (withdrawAll) {
+        result = await sendCoinsAll({ address });
+      }
+      else {
+        result = await sendCoins({ address, sat: Number.parseInt(sat, 10)});
+      }
       console.log(result);
       await getBalance(undefined);
       navigation.pop();
@@ -62,6 +67,16 @@ export default ({ navigation }: IOpenChannelProps) => {
     });
   };
 
+  const onWithdrawAllPress = () => {
+    setSat("Withdraw all funds");
+    setWithdrawAll(true);
+  };
+
+  const onCancelWithdrawAllPress = () => {
+    setSat("");
+    setWithdrawAll(false);
+  };
+
   return (
     <Container>
       <Header iosBarStyle="light-content" translucent={false}>
@@ -83,11 +98,19 @@ export default ({ navigation }: IOpenChannelProps) => {
               <Input placeholder="Bitcoin address" value={address} onChangeText={onAddressChange} />
               <Icon type="AntDesign" name="camera" onPress={onCameraPress} />
             </>
-          )
+          ),
         }, {
           key: "AMOUNT",
           title: "Amount",
-          component: (<Input placeholder="Amount (satoshi)" keyboardType="numeric" onChangeText={setSat} value={sat} />)
+          component: (
+            <>
+              <Input placeholder="Amount (satoshi)" keyboardType="numeric" onChangeText={setSat} value={withdrawAll ? "Withdraw all funds" : sat} disabled={withdrawAll} />
+              {!withdrawAll
+                ? <Button onPress={onWithdrawAllPress} style={{ marginRight: 5 }} small={true}><Text>All</Text></Button>
+                : <Button onPress={onCancelWithdrawAllPress} style={{ marginRight: 5 }} small={true}><Text>x</Text></Button>
+              }
+            </>
+          ),
         }]}
         buttons={[
           <Button key="WITHDRAW" onPress={onWithdrawClick} block={true} primary={true}>
