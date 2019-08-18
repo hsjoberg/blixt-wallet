7c54fec30bedcafa25b22e40ceffb169a19ec90f
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-08-18 16:01:58 +0200

    Modal show Lightning Node Info

---
+++ src/windows/Settings/LightningNodeInfo.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Clipboard } from "react-native";
+import { Body, Card, Text, CardItem, H1, Toast } from "native-base";
+import { NavigationScreenProp, ScrollView } from "react-navigation";
+import { fromUnixTime } from "date-fns";
+
+import Blurmodal from "../../components/BlurModal";
+import { formatISO } from "../../utils";
+import { useStoreState, useStoreActions } from "../../state/store";
+
+interface IMetaDataProps {
+  title: string;
+  data: string | string[];
+}
+const MetaData = ({ title, data }: IMetaDataProps) => {
+  return (
+    <Text
+      style={style.detailText}
+      onPress={() => {
+        Array.isArray(data)
+          ? Clipboard.setString(data.join("\n"))
+          : Clipboard.setString(data);
+
+        Toast.show({ text: "Copied to clipboard.", type: "warning" });
+      }}
+    >
+      <Text style={{ fontWeight: "bold" }}>{title}:{"\n"}</Text>
+      {Array.isArray(data) && data.join("\n")}
+      {!Array.isArray(data) && data}
+    </Text>
+  );
+};
+
+export interface ITransactionDetailsProps {
+  navigation: NavigationScreenProp<{}>;
+}
+export default ({ navigation }: ITransactionDetailsProps) => {
+  const nodeInfo = useStoreState((store) => store.lightning.nodeInfo);
+  const getNodeInfo = useStoreActions((store) => store.lightning.getInfo);
+
+  useEffect(() => {
+    (async () => {
+      await getNodeInfo();
+    })();
+  }, [getNodeInfo]);
+
+  if (!nodeInfo) {
+    return (<></>);
+  }
+
+  return (
+    <Blurmodal navigation={navigation}>
+      <Card style={style.card}>
+        <CardItem>
+          <Body>
+            <ScrollView>
+              <H1 style={style.header}>Node Info</H1>
+              <MetaData title="Node Alias" data={nodeInfo.alias!} />
+              <MetaData title="Chain" data={nodeInfo.chains!.map(({chain, network}, key) => `${chain} (${network})`).join("\n")} />
+              <MetaData title="Best Header Timestamp" data={formatISO(fromUnixTime(nodeInfo.bestHeaderTimestamp!.toNumber()))} />
+              <MetaData title="Block Hash" data={nodeInfo.blockHash!} />
+              <MetaData title="Block Height" data={nodeInfo.blockHeight!.toString()} />
+              <MetaData title="Identity Pubkey" data={nodeInfo.identityPubkey!} />
+              <MetaData title="Num Channels" data={[
+                `Active: ${nodeInfo.numActiveChannels!.toString()}`,
+                `Inactive: ${nodeInfo.numInactiveChannels!.toString()}`,
+                `Pending: ${nodeInfo.numPendingChannels!.toString()}`,
+              ]} />
+              <MetaData title="Num peers" data={nodeInfo.numPeers!.toString()} />
+              <MetaData title="Synced to Chain" data={nodeInfo.syncedToChain!.toString()} />
+              <MetaData title="Testnet" data={nodeInfo.testnet!.toString()} />
+              {nodeInfo.uris && nodeInfo.uris.length > 0 && <MetaData title="Node URIs" data={nodeInfo.uris.join("\n")} />}
+              <MetaData title="Lnd Version" data={nodeInfo.version!} />
+            </ScrollView>
+          </Body>
+        </CardItem>
+      </Card>
+    </Blurmodal>
+  );
+};
+
+const style = StyleSheet.create({
+  card: {
+    padding: 5,
+    width: "100%",
+    minHeight: "55%",
+    maxHeight: "94%",
+  },
+  header: {
+    width: "100%",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  detailText: {
+    marginBottom: 7,
+  },
+});
+
