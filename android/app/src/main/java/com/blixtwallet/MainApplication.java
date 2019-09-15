04f003557acd944a8bd7f4ab5a9136582d94763c
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2019-09-15 12:59:45 +0200

    Fixing app not working on Dalvik runtime

--- android/app/src/main/java/com/blixtwallet/MainApplication.java
+++ android/app/src/main/java/com/blixtwallet/MainApplication.java
@@ -1,6 +1,6 @@
 package com.blixtwallet;
 
-import android.app.Application;
+import androidx.multidex.MultiDexApplication;
 import android.content.Context;
 
 import com.facebook.react.PackageList;
@@ -11,7 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
