622d925f22015cca214103ff44f93df17d1d1c78
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2020-01-23 03:31:49 +0100

    Java backend now uses /proto for compiling

--- android/app/src/main/java/com/blixtwallet/LndMobileService.java
+++ android/app/src/main/java/com/blixtwallet/LndMobileService.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 
-import io.grpc.LightningGrpc.Rpc;
+import lnrpc.Rpc;
 import com.google.protobuf.ByteString;
 
 import com.hypertrack.hyperlog.HyperLog;
