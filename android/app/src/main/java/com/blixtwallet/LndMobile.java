12b5746b36169df56695c7e91752a03645d67008
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2020-02-06 15:00:13 +0100

    Handle sharing invoice to app
    Both normal intents and NFC.
    Supports `lightning:` URIs and plan/text mime-type.

--- android/app/src/main/java/com/blixtwallet/LndMobile.java
+++ android/app/src/main/java/com/blixtwallet/LndMobile.java
@@ -18,6 +18,15 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 
+import android.nfc.Tag;
+import android.nfc.NfcAdapter;
+import android.nfc.NdefMessage;
+import android.nfc.tech.Ndef;
+import android.nfc.NdefRecord;
+import android.os.Parcelable;
+import java.util.Arrays;
+import java.io.UnsupportedEncodingException;
+
 import java.io.PrintWriter;
 import java.io.File;
 import java.io.InputStream;
@@ -665,6 +674,91 @@ void deleteRecursive(File fileOrDirectory) {
     };
 
     permissions.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, checkPermissionPromise);
+  }
+
+  @ReactMethod
+  public void startLndLogObserving() {
+    FileObserver lndLogObserver;
+    File lndLog = new File(
+      getReactApplicationContext().getFilesDir().toString() +
+      "/logs/bitcoin/" +
+      BuildConfig.CHAIN +
+      "/lnd.log"
+    );
+    // TODO
+  }
+
+  @ReactMethod
+  public void getIntentStringData(Promise promise) {
+    String sharedText = getReactApplicationContext()
+      .getCurrentActivity().getIntent().getStringExtra(Intent.EXTRA_TEXT);
+
+    if (sharedText != null) {
+      Log.d(TAG, sharedText);
+      promise.resolve(sharedText);
+    }
+    else {
+      Log.d(TAG, "sharedText null");
+      promise.resolve(null);
+    }
+  }
+
+  @ReactMethod
+  public void getIntentNfcData(Promise promise) {
+    // https://code.tutsplus.com/tutorials/reading-nfc-tags-with-android--mobile-17278
+    Tag tag = getReactApplicationContext()
+      .getCurrentActivity().getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+    if (tag == null) {
+      promise.resolve(null);
+      return;
+    }
+
+    Ndef ndef = Ndef.get(tag);
+    if (ndef == null) {
+      HyperLog.d(TAG, "NFC tag is not NDEF");
+      promise.resolve(null);
+    }
+
+    NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+    NdefRecord[] records = ndefMessage.getRecords();
+    if (records.length > 0) {
+      // Get first record and ignore the rest
+      NdefRecord record = records[0];
+      if (record.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)) {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+        */
+        byte[] payload = record.getPayload();
+
+        // Get the Text Encoding
+        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+        // Get the Language Code
+        int languageCodeLength = payload[0] & 0063;
+
+        // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+        // e.g. "en"
+
+        try {
+          String s = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+          promise.resolve(s);
+          return;
+        } catch (UnsupportedEncodingException e) {
+          HyperLog.e(TAG, "Error returning ndef data", e);
+        }
+      }
+      else {
+        HyperLog.d(TAG, "Cannot read NFC Tag Record");
+      }
+    }
+    promise.resolve(null);
   }
 }
 
--- src/windows/InitProcess/InitLightning.tsx
+++ src/windows/InitProcess/InitLightning.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
-import { StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar, Linking, NativeModules } from "react-native";
 import { Spinner, H1, H3 } from "native-base";
 import { NavigationScreenProp } from "react-navigation";
+import * as Base64 from "base64-js";
 
 import { useStoreActions, useStoreState } from "../../state/store";
 import { blixtTheme } from "../../../native-base-theme/variables/commonColor";
 import Container from "../../components/Container";
+import { timeout, bytesToString } from "../../utils";
 
 interface IProps {
   navigation: NavigationScreenProp<{}>;
@@ -15,16 +17,44 @@ export default ({ navigation }: IProps) => {
   const nodeInfo = useStoreState((store) => store.lightning.nodeInfo);
   const firstSync = useStoreState((store) => store.lightning.firstSync);
   const ready = useStoreState((store) => store.lightning.ready);
+  const rpcReady = useStoreState((store) => store.lightning.rpcReady);
+  const setPayment = useStoreActions((store) => store.send.setPayment);
 
   useEffect(() => {
     (async () => {
-      await initializeLightning(undefined);
+      await initializeLightning();
       console.log("initializeLightning() done");
     })();
   }, [initializeLightning]);
 
   if (ready) {
+    // Check for deep links
     setTimeout(async () => {
+      try {
+        let lightningURI = await Linking.getInitialURL();
+        if (lightningURI === null) {
+          lightningURI = await NativeModules.LndMobile.getIntentStringData();
+        }
+        if (lightningURI === null) {
+          lightningURI = await NativeModules.LndMobile.getIntentNfcData();
+        }
+
+        if (lightningURI && lightningURI.startsWith("lightning:")) {
+          console.log("try lightningURI");
+
+          while (!rpcReady) {
+            await timeout(500);
+          }
+
+          await setPayment({
+            paymentRequestStr: lightningURI.toUpperCase().replace("LIGHTNING:", ""),
+          });
+          navigation.navigate("SendConfirmation");
+          return;
+        }
+      } catch (e) {
+        console.log(e.message);
+      }
       navigation.navigate("Main");
     }, 0);
   }
