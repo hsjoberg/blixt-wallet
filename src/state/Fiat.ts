c62c0f47f9aa199a5867cdc2eb3b98bcde492328
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2020-08-10 02:32:50 +0200

    Add unique icons for debug builds

--- src/state/Fiat.ts
+++ src/state/Fiat.ts
@@ -5,7 +5,8 @@ import logger from "./../utils/log";
 const log = logger("Fiat");
 
 const BLOCKCHAIN_FIAT_API_URL = "https://blockchain.info/ticker";
-const BTCSAT = 100000000;
+const COINGECKO_CURRENCIES ="usd,eur,gbp,sek,aed,ars,aud,bdt,bhd,bmd,brl,cad,chf,clp,cny,czk,dkk,hkd,huf,idr,ils,inr,jpy,krw,kwd,lkr,mmk,mxn,myr,nok,nzd,php,pkr,pln,rub,sar,sgd,thb,try,twd,uah,vef,vnd,zar,xdr,xag,xau";
+const COINGECKO_FIAT_API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=" + COINGECKO_CURRENCIES;
 
 export interface IFiatRate {
   last: number;
@@ -35,6 +36,58 @@ export interface IFiatRates {
   TWD: IFiatRate;
 }
 
+interface ICoingeckoPriceResponse {
+  bitcoin: {
+    usd: number,
+    aed: number;
+    ars: number;
+    aud: number;
+    bdt: number;
+    bhd: number;
+    bmd: number;
+    brl: number;
+    cad: number;
+    chf: number;
+    clp: number;
+    cny: number;
+    czk: number;
+    dkk: number;
+    eur: number;
+    gbp: number;
+    hkd: number;
+    huf: number;
+    idr: number;
+    ils: number;
+    inr: number;
+    jpy: number;
+    krw: number;
+    kwd: number;
+    lkr: number;
+    mmk: number;
+    mxn: number;
+    myr: number;
+    nok: number;
+    nzd: number;
+    php: number;
+    pkr: number;
+    pln: number;
+    rub: number;
+    sar: number;
+    sek: number;
+    sgd: number;
+    thb: number;
+    try: number;
+    twd: number;
+    uah: number;
+    vef: number;
+    vnd: number;
+    zar: number;
+    xdr: number;
+    xag: number;
+    xau: number;
+  }
+}
+
 export interface IFiatModel {
   getRate: Thunk<IFiatModel>;
 
@@ -47,14 +100,28 @@ export interface IFiatModel {
 export const fiat: IFiatModel = {
   getRate: thunk(async (actions) => {
     try {
-      log.d("Fetching fiat rate");
+      log.d("Fetching fiat rate from Coingecko");
+      const result = await fetch(COINGECKO_FIAT_API_URL);
+      const jsonResult = await result.json() as ICoingeckoPriceResponse;
+      const parsed = {} as any;
+      for (const [currency, rate] of Object.entries(jsonResult.bitcoin)) {
+        parsed[currency.toUpperCase()] = { last: rate };
+      }
+      actions.setFiatRates(parsed);
+    } catch (e) {
+      log.e("Failed to fetch fiat rate from Coingecko: " + e.message);
+      log.i("Falling back to Blockchain.info");
+    }
+
+    try {
+      log.d("Fetching fiat rate from Blockchain.info");
       const result = await fetch(BLOCKCHAIN_FIAT_API_URL);
       const jsonResult = await result.json();
       if (validateFiatApiResponse(jsonResult)) {
         actions.setFiatRates(jsonResult);
       }
     } catch (e) {
-      log.e("Failed to fetch fiat rate API:" + e.message);
+      log.e("Failed to fetch fiat rate from Blockchain.info: " + e.message);
     }
   }),
 
