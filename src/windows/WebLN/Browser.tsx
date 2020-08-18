47a3065ba8f43e01630f8e3cb4a677a15577a2d4
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2020-08-18 22:05:46 +0200

    Removing blur from WebLN browser modal
    RealtimeBlurView/Native UI module is causing slowdown for some reason

--- src/windows/WebLN/Browser.tsx
+++ src/windows/WebLN/Browser.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
 import { Alert, TextInput, StatusBar, StyleSheet, View, TouchableOpacity, BackHandler, ToastAndroid } from "react-native";
+import Modal from "react-native-modal";
 import { injectJs, onMessageHandler } from "react-native-webln";
 import { RouteProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -115,72 +116,70 @@ export default function WebLNBrowser({ navigation, route }: IBrowserProps) {
   }
 
   return (
-    <BlurModal useModalComponent={false}>
-      <Card style={style.card}>
-        <WebView
-          containerStyle={{ opacity: showWebview ? 1 : 0 }}
-          ref={webview}
-          onMessage={onMessage}
-          userAgent="BlixtWallet/alpha (WebLN)"
-          source={{ uri: url }}
-          onLoadStart={(e) => {
-            console.log("onLoadStart");
-            setJsInjected(false)
+    <Card style={style.card}>
+      <WebView
+        containerStyle={{ opacity: showWebview ? 1 : 0 }}
+        ref={webview}
+        onMessage={onMessage}
+        userAgent="BlixtWallet/alpha (WebLN)"
+        source={{ uri: url }}
+        onLoadStart={(e) => {
+          console.log("onLoadStart");
+          setJsInjected(false)
+        }}
+        onLoadProgress={(e) => {
+          if (!jsInjected && e.nativeEvent.progress > 0.75) {
+            webview.current!.injectJavaScript(injectJs());
+            setJsInjected(true);
+            console.log("Injected");
+          }
+        }}
+        onLoadEnd={() => requestAnimationFrame(() => setShowWebview(true))}
+        onNavigationStateChange={(e) => {
+          if (canGoBack !== (e.url !== initialUrl)) {
+            setCanGoBack(e.url !== initialUrl);
+          }
+          setUrl(e.url);
+          setUrlInput(e.url);
+          if (textInput.current) {
+            textInput.current.blur();
+          }
+        }}
+        onError={(e) => console.log(e)}
+        style={style.webview}
+      />
+      <View style={style.urlNav}>
+        <TouchableOpacity onPress={closeBrowser}>
+          <Icon style={style.closeButton} type="AntDesign" name="close" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setUrl(INITIAL_URL)} onLongPress={() => ToastAndroid.show("Go back to store list", ToastAndroid.SHORT)}>
+          <Icon style={style.listButton} type="FontAwesome5" name="home" />
+        </TouchableOpacity>
+        <TextInput
+          ref={textInput}
+          style={style.urlInput}
+          value={urlInput}
+          autoCapitalize="none"
+          onChangeText={(text) => {
+            setUrlInput(text);
           }}
-          onLoadProgress={(e) => {
-            if (!jsInjected && e.nativeEvent.progress > 0.75) {
-              webview.current!.injectJavaScript(injectJs());
-              setJsInjected(true);
-              console.log("Injected");
+          onSubmitEditing={(e) => {
+            if (!e.nativeEvent.text.startsWith("https://") && !e.nativeEvent.text.startsWith("http://")) {
+              e.nativeEvent.text = "https://" + e.nativeEvent.text;
             }
+            setUrl(e.nativeEvent.text);
           }}
-          onLoadEnd={() => requestAnimationFrame(() => setShowWebview(true))}
-          onNavigationStateChange={(e) => {
-            if (canGoBack !== (e.url !== initialUrl)) {
-              setCanGoBack(e.url !== initialUrl);
-            }
-            setUrl(e.url);
-            setUrlInput(e.url);
-            if (textInput.current) {
-              textInput.current.blur();
-            }
-          }}
-          onError={(e) => console.log(e)}
-          style={style.webview}
+          keyboardType="url"
+          placeholder="Type URL here..."
         />
-        <View style={style.urlNav}>
-          <TouchableOpacity onPress={closeBrowser}>
-            <Icon style={style.closeButton} type="AntDesign" name="close" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setUrl(INITIAL_URL)} onLongPress={() => ToastAndroid.show("Go back to store list", ToastAndroid.SHORT)}>
-            <Icon style={style.listButton} type="FontAwesome5" name="home" />
-          </TouchableOpacity>
-          <TextInput
-            ref={textInput}
-            style={style.urlInput}
-            value={urlInput}
-            autoCapitalize="none"
-            onChangeText={(text) => {
-              setUrlInput(text);
-            }}
-            onSubmitEditing={(e) => {
-              if (!e.nativeEvent.text.startsWith("https://") && !e.nativeEvent.text.startsWith("http://")) {
-                e.nativeEvent.text = "https://" + e.nativeEvent.text;
-              }
-              setUrl(e.nativeEvent.text);
-            }}
-            keyboardType="url"
-            placeholder="Type URL here..."
-          />
-          <TouchableOpacity onPress={() => webview.current!.reload()}>
-            <Icon style={style.goButton} type="MaterialCommunityIcons" name="sync" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => setUrl(urlInput)}>
-            <Icon style={style.goButton} type="AntDesign" name="arrowright" />
-          </TouchableOpacity>
-        </View>
-      </Card>
-    </BlurModal>
+        <TouchableOpacity onPress={() => webview.current!.reload()}>
+          <Icon style={style.goButton} type="MaterialCommunityIcons" name="sync" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setUrl(urlInput)}>
+          <Icon style={style.goButton} type="AntDesign" name="arrowright" />
+        </TouchableOpacity>
+      </View>
+    </Card>
   );
 }
 
