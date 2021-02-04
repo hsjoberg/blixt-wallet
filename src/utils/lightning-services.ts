b3d322b113f7032684c9ef3533bef849d03fdab0
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-02-04 13:15:35 +0100

    Add LNBIG pubkeys to lightning-services.ts

--- src/utils/lightning-services.ts
+++ src/utils/lightning-services.ts
@@ -151,6 +151,35 @@ export function identifyService(pubkey: string | null, description: string, webs
   if (pubkey === "021ae2986faa33f82ef4106f9ac2265f9fd9fb2061ffd6028ab6782ebfa3d8bc1f" || website?.includes("chat.blixtwallet.com")) {
     return "lnurlpaychat";
   }
+  if ([
+    "0390b5d4492dc2f5318e5233ab2cebf6d48914881a33ef6a9c6bcdbb433ad986d0", // LND-01
+    "03d37fca0656558de4fd86bbe490a38d84a46228e7ec1361801f54f9437a18d618", // LND-02
+    "032679fec1213e5b0a23e066c019d7b991b95c6e4d28806b9ebd1362f9e32775cf", // LND-03
+    "031ce29116eab7edd66148f5169f1fb658fad62bdc5091221ab895fe5d36db00b2", // LND-05
+    "03bc9337c7a28bb784d67742ebedd30a93bacdf7e4ca16436ef3798000242b2251", // LND-06
+    "03da1c27ca77872ac5b3e568af30673e599a47a5e4497f85c7b5da42048807b3ed", // LND-07
+    "02de11c748f5b25cfd2ce801176d3926bfde4de23b1ff43e692a5b76cf06805e4a", // LND-09
+    "02bb24da3d0fb0793f4918c7599f973cc402f0912ec3fb530470f1fc08bdd6ecb5", // LND-10
+    "033e9ce4e8f0e68f7db49ffb6b9eecc10605f3f3fcb3c630545887749ab515b9c7", // LND-11
+    "034ea80f8b148c750463546bd999bf7321a0e6dfc60aaf84bd0400a2e8d376c0d5", // LND-12
+    "035f5236d7e6c6d16107c1f86e4514e6ccdd6b2c13c2abc1d7a83cd26ecb4c1d0e", // LND-13
+    "03fb822818be083e0a954db85257a2911a3d55458b8c1ea4124b157e865a836d12", // LND-17
+    "02c91d6aa51aa940608b497b6beebcb1aec05be3c47704b682b3889424679ca490", // LND-21
+    "0303a518845db99994783f606e6629e705cfaf072e5ce9a4d8bf9e249de4fbd019", // LND-25
+    "02247d9db0dfafea745ef8c9e161eb322f73ac3f8858d8730b6fd97254747ce76b", // LND-26
+    "03fce165537aea120bffe8505876b44d5119354f825b3eac329b761fc5636bf334", // LND-27
+    "030995c0c0217d763c2274aa6ed69a0bb85fa2f7d118f93631550f3b6219a577f5", // LND-28
+    "03e5ea100e6b1ef3959f79627cb575606b19071235c48b3e7f9808ebcd6d12e87d", // LND-31
+    "039edc94987c8f3adc28dab455efc00dea876089a120f573bd0b03c40d9d3fb1e1", // LND-32
+    "028a8e53d70bc0eb7b5660943582f10b7fd6c727a78ad819ba8d45d6a638432c49", // LND-33
+    "022755c3ff4e5a1d71f573cda4b315887fc00a9e5c9ea9a847d939f3e517e69a70", // LND-34
+    "022c260f9ad58196af280c80a96ec9eabf6404df59ff1a7553b0f381c875a29ba0", // LND-37
+    "024d2387409269f3b79e2708bb39b895c9f4b6a8322153af54eba487d4993bf60f", // LND-38
+    "03dab87ff8635982815c4567eb58af48f9944d11c56beb12b91e1049aaea06e187", // LND-41
+    "0311cad0edf4ac67298805cf4407d94358ca60cd44f2e360856f3b1c088bcd4782", // LND-42
+  ].includes(pubkey ?? "")) {
+    return "lnbig";
+  }
   else if (description.includes("Hampus Sjöberg") || description.includes("Hampus Sjoberg")) {
     return "hampus";
   }
