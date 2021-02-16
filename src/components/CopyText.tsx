a22100a6e5a81b6a4087540e279d694d87bf418e
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-02-16 14:17:57 +0100

    Fix missing CopyTetxt.tsx file

---
+++ src/components/CopyText.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TextStyle } from "react-native";
+import { NativeBase, Text } from "native-base";
+import Clipboard from "@react-native-community/clipboard";
+
+import { toast } from "../utils";
+
+export interface ICopyTextProps extends NativeBase.Text {
+  children?: string | null;
+}
+export default function CopyText(props: ICopyTextProps) {
+  const onPress = () => {
+    Clipboard.setString(props.children ?? "");
+    toast("Copied to clipboard");
+  }
+
+  return (
+    <Text onPress={onPress} {...props}></Text>
+  );
+};
