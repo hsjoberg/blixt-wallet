d105a2aef7621dcae9ca5b9206a2d484224ff6cc
Simon B <simon.bohlin@gmail.com>
2021-02-28 23:16:47 +0100

    WIP7

--- build_aar/build_aar.sh
+++ build_aar/build_aar.sh
@@ -5,12 +5,16 @@ gomobile init
 GO111MODULE=on go get github.com/golang/protobuf/protoc-gen-go@v1.3.2
 GO111MODULE=on go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@v1.14.3
 GO111MODULE=on go get github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@v1.14.3
-GO111MODULE=on go get -u -v github.com/lightninglabs/falafel
-go get -d github.com/lightningnetwork/lnd
-cd $GOPATH/src/github.com/lightningnetwork/lnd
-cd $GOPATH/src/github.com/lightningnetwork/lnd && git checkout v0.12.0-beta && sed -i 's/use_prefix="0"/use_prefix="1"/g' mobile/gen_bindings.sh && make android tags="routerrpc walletrpc signrpc invoicesrpc"
-
 ls
 ls /
-
+GO111MODULE=on go get -u -v github.com/lightninglabs/falafel
+go get -d github.com/lightningnetwork/lnd
+ls
+ls /
+cd $GOPATH/src/github.com/lightningnetwork/lnd
+ls
+ls /
+cd $GOPATH/src/github.com/lightningnetwork/lnd && git checkout v0.12.0-beta && sed -i 's/use_prefix="0"/use_prefix="1"/g' mobile/gen_bindings.sh && make android tags="routerrpc walletrpc signrpc invoicesrpc"
+ls
+ls /
 cp $GOPATH/src/github.com/lightningnetwork/lnd/mobile/build/android/Lndmobile.aar /
