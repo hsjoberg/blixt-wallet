1cc23dcda55b12b2af5c533cb505c7057a987505
Simon B <simon.bohlin@tretton37.com>
2021-03-07 10:20:40 +0100

    Comment out dead code.

--- src/native-base-theme/components/Icon.js
+++ src/native-base-theme/components/Icon.js
@@ -5,7 +5,7 @@ import variable, { blixtTheme } from './../variables/commonColor';
 export default (variables /* : * */ = variable) => {
   const iconTheme = {
     fontSize: variables.iconFontSize,
-    color: variable.textColor,
+    // color: variable.textColor,
 
     // BLIXT:
     color: blixtTheme.light,
