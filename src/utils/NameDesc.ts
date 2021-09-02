e04e595ada0ab600ab44abd3195254e75edcdbff
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2021-09-02 11:20:38 +0200

    Fix typo in default NameDesc description text

--- src/utils/NameDesc.ts
+++ src/utils/NameDesc.ts
@@ -8,7 +8,7 @@ export interface INameDesc {
 export const setupDescription = (description: string, name: string | null) => {
   let d = description;
   if (name !== null) {
-    d = name + ":  " + (description || "No invoice descripion");
+    d = name + ":  " + (description || "No invoice description");
   }
   return d;
 }
