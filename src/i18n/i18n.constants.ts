3735595f00251ecf5b3e991953d79e92cf335c13
Juan <>
2021-12-20 00:09:22 +0100

    add EN locales

--- src/i18n/i18n.constants.ts
+++ src/i18n/i18n.constants.ts
@@ -1,8 +1,82 @@
 export const namespaces = {
   common: "common",
-  settings: {
-    settings: "settings.settings"
+  contacts:{
+    contactList:"contacts.contactList"
   },
+  google:{
+    googleDriveTestbed:"google.googleDriveTestbed"
+  },
+  initProcess:{
+    initLightning:"initProcess.initLightning"
+  },
+  keysend:{
+    experiment:"keysend.experiment"
+  },
+  lightningInfo:{
+    lightningInfo:"lightningInfo.lightningInfo",
+    openChannel:"lightningInfo.openChannel"
+  },
+  LNURL:{
+    payRequest:{
+      payerData:"LNURL.payRequest.payerData",
+      paymentCard:"LNURL.payRequest.paymentCard",
+      paymentDone:"LNURL.payRequest.paymentDone"
+    },
+    authRequest:"LNURL.authRequest",
+    channelRequest:"LNURL.channelRequest",
+    LNURLPayRequest:"LNURL.LNURLPayRequest",
+    payRequestAboutLightningAddress:"LNURL.payRequestAboutLightningAddress",
+    withdrawRequest:"LNURL.withdrawRequest"
+  },
+  onchain:{
+    onChainInfo:"onchain.onChainInfo",
+    onChainTransactionDetails:"onchain.onChainTransactionDetails",
+    onChainTransactionLog:"onchain.onChainTransactionLog",
+    withdraw:"onchain.withdraw"
+  },
+  receive:{
+    dunderLspInfo:"receive.dunderLspInfo",
+    receiveQr:"receive.receiveQr",
+    receiveSetup:"receive.receiveSetup",
+    receiveSetupLsp:"receive.receiveSetupLsp"
+  },
+  send:{
+    sendCamera:"send.sendCamera",
+    sendConfirmation:"send.sendConfirmation",
+    sendDone:"send.sendDone"
+  },
+  settings: {
+    about:"settings.about",
+    settings: "settings.settings",
+    connectToLightningPeer:"settings.connectToLightningPeer",
+    dunderDoctor:"settings.dunderDoctor",
+    lightningNodeInfo: "settings.lightningNodeInfo",
+    lightningPeers:"settings.lightningPeers",
+    lndLog:"settings.lndLog",
+    lndMobileHelpCenter:"settings.lndMobileHelpCenter",
+    removePincodeAuth:"settings.removePincodeAuth",
+    setPincode:"settings.setPincode",
+    torShowOnionAddress:"settings.torShowOnionAddress",
+  },
+  web:{
+    info:"web.info"
+  },
+  webLN:{
+    browser:"webLN.browser"
+  },
+  welcome:{
+    addFunds:"welcome.addFunds",
+    almostDone:"welcome.almostDone",
+    confirm:"welcome.confirm",
+    googleDriveBackup:"welcome.googleDriveBackup",
+    iCloudBackup:"welcome.iCloudBackup",
+    restore:"welcome.restore",
+    seed:"welcome.seed",
+    start:"welcome.start"
+  },
+  help:"help",
+  overview:"overview",
+  syncInfo:"syncInfo"
 };
 
 export const languages = {
