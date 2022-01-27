9fcc56a7dc25237bc14b85e6490adb9e044c009c
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2022-01-27 22:05:32 +0100

    lightning-services: Add Kollider

--- contrib/service-image/convert.sh
+++ contrib/service-image/convert.sh
@@ -7,6 +7,6 @@ do
   echo ""
   title=$(echo $f | sed 's/\.[^.]*$//')
   echo "Processing $f..."
-  cwebp "$f" -q 30 -resize 225 225 -o webp/"$(echo $f | cut -f 1 -d ".").webp"
+  cwebp "$f" -q 30 -resize 225 225 -o webp/"$(echo $f | rev | cut -d"." -f2- | rev).webp" < /dev/null
   echo ""
 done
