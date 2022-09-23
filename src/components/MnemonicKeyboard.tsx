0fd4f163173ed28417d372e9ad11f5cd04173815
ar563 <98338498+ar563@users.noreply.github.com>
2022-09-23 02:07:35 +0200

    Update MnemonicKeyboard.tsx

--- src/components/MnemonicKeyboard.tsx
+++ src/components/MnemonicKeyboard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-export default function App() {
+export default function MnemonicKeyboard() {
   return (
     <View>
       <View style={styles.row}>
