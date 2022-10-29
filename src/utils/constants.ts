a461a9a670dcadc46d948f4ef466449f83092da8
Juan P Lopez <dolcalmi@gmail.com>
2022-10-29 15:19:15 -0500

    Receive: remove MAX_SAT_INVOICE

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -8,8 +8,6 @@ export const TLV_RECORD_NAME = 128101;
 export const TLV_KEYSEND = 5482373484;
 export const TLV_WHATSAT_MESSAGE = 34349334;
 
-export const MAX_SAT_INVOICE = 4294967;
-
 export const GITHUB_REPO_URL = "https://github.com/hsjoberg/blixt-wallet";
 export const HAMPUS_EMAIL = "mailto:hampus.sjoberg💩protonmail.com".replace("💩", "@");
 export const TELEGRAM = "https://t.me/blixtwallet";
