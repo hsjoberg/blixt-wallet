628e7e70345f10e1b3dfbb6eda60be04051cb193
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2022-12-20 11:35:36 +0100

    web: fix Sql.js not always throwing an Error (sometimes a string...)

--- src/storage/database/db-utils.web.ts
+++ src/storage/database/db-utils.web.ts
@@ -1,31 +1,58 @@
 import { SqlJs } from "sql.js/module";
 
 export const query = async (db: SqlJs.Database, sql: string, params: any[]) => {
-  const r = await db.exec(sql, params);
-  return r;
+  try {
+    return await db.exec(sql, params);
+  } catch (e) {
+    if (typeof e === "string") {
+      throw new Error(e);
+    }
+    throw e;
+  }
 };
 
 /**
  * @returns number Insert ID
  */
 export const queryInsert = async (db: SqlJs.Database, sql: string, params: any[]): Promise<number> => {
-  await query(db, sql, params);
+  try {
+    await query(db, sql, params);
 
-  const r = await query(db, "SELECT last_insert_rowid() AS id", []);
-  return r[0].values[0][0] as number; // TODO
+    const r = await query(db, "SELECT last_insert_rowid() AS id", []);
+    return r[0].values[0][0] as number; // TODO
+  } catch (e) {
+    if (typeof e === "string") {
+      throw new Error(e);
+    }
+    throw e;
+  }
 };
 
 export const queryMulti = async <T>(db: SqlJs.Database, sql: string, params: any[] = []): Promise<T[]> => {
-  const r = await query(db, sql, params);
-  return convertToKeyValue(r[0]) as T[];
+  try {
+    const r = await query(db, sql, params);
+    return convertToKeyValue(r[0]) as T[];
+  } catch (e) {
+    if (typeof e === "string") {
+      throw new Error(e);
+    }
+    throw e;
+  }
 };
 
 export const querySingle = async <T>(db: SqlJs.Database, sql: string, params: any[]): Promise<T | null> => {
-  const r = await query(db, sql, params);
-  if (r[0]) {
-    return convertToKeyValue(r)[0] as T;
+  try {
+    const r = await query(db, sql, params);
+    if (r[0]) {
+      return convertToKeyValue(r)[0] as T;
+    }
+    return null;
+  } catch (e) {
+    if (typeof e === "string") {
+      throw new Error(e);
+    }
+    throw e;
   }
-  return null;
 };
 
 // https://github.com/sql-js/sql.js/issues/84#issuecomment-155982147
