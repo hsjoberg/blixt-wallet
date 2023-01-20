21deefccbd229719a20f0c74a4d11942b1ff0dd5
djkazic <kevincai96@gmail.com>
2023-01-20 17:21:32 -0500

    Use TorService JNI unix socket to provide tor connectivity to LND

--- android/app/src/main/java/com/blixtwallet/BlixtTorUtils.java
+++ android/app/src/main/java/com/blixtwallet/BlixtTorUtils.java
@@ -14,17 +14,6 @@ public class BlixtTorUtils {
     return socksPort;
   }
 
-  public static int getControlPort() {
-    int controlPort = 9071;
-    if (BuildConfig.CHAIN.equals("testnet")) {
-      controlPort += 10;
-    }
-    if (BuildConfig.DEBUG) {
-      controlPort += 100;
-    }
-    return controlPort;
-  }
-
   public static int getListenPort() {
     int listenPort = 9760;
     if (BuildConfig.CHAIN.equals("testnet")) {
