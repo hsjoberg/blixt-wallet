e9dc0c451ca9b48dfb0c7eecefd409e6e702c790
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2023-11-29 17:46:44 +0100

    Update node modules

--- jestSetup.js
+++ jestSetup.js
@@ -1,17 +1,31 @@
 import React from "react";
-jest.mock("@react-native-async-storage/async-storage", () => require("./mocks/@react-native-community/async-storage"));
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("./mocks/@react-native-community/async-storage"),
+);
 jest.mock("react-native-sqlite-storage", () => require("./mocks/react-native-sqlite-storage"));
 jest.mock("react-native-build-config", () => require("./mocks/react-native-build-config"));
-jest.mock("react-native-push-notification", () => require("./mocks/react-native-push-notification"));
+jest.mock("react-native-push-notification", () =>
+  require("./mocks/react-native-push-notification"),
+);
 jest.mock("react-native-keychain", () => require("./mocks/react-native-keychain"));
 jest.mock("react-native-securerandom", () => require("./mocks/react-native-securerandom"));
-jest.mock("react-native-fingerprint-scanner", () => require("./mocks/react-native-fingerprint-scanner"));
-jest.mock("@react-native-community/clipboard", () => require("./mocks/@react-native-community/clipboard"));
-jest.mock("@react-native-community/masked-view", () => require("./mocks/@react-native-community/masked-view"));
-jest.mock("@react-native-google-signin/google-signin", () => require("./mocks/@react-native-community/google-signin"));
+jest.mock("react-native-fingerprint-scanner", () =>
+  require("./mocks/react-native-fingerprint-scanner"),
+);
+jest.mock("@react-native-clipboard/clipboard", () =>
+  require("./mocks/@react-native-community/clipboard"),
+);
+jest.mock("@react-native-community/masked-view", () =>
+  require("./mocks/@react-native-community/masked-view"),
+);
+jest.mock("@react-native-google-signin/google-signin", () =>
+  require("./mocks/@react-native-community/google-signin"),
+);
 jest.mock("react-native-fs", () => require("./mocks/react-native-fs"));
 jest.mock("react-native-document-picker", () => require("./mocks/react-native-document-picker"));
-jest.mock("@react-native-community/geolocation", () => require("./mocks/@react-native-community/geolocation"));
+jest.mock("@react-native-community/geolocation", () =>
+  require("./mocks/@react-native-community/geolocation"),
+);
 jest.mock("react-native-permissions", () => require("./mocks/react-native-permissions"));
 
 jest.mock("./src/lndmobile/index", () => require("./mocks/lndmobile/index"));
@@ -29,18 +43,18 @@ ReactNative.NativeModules.LndMobileTools = {};
 ReactNative.NativeModules.LndMobileTools.log = jest.fn();
 ReactNative.UIManager.configureNext = jest.fn();
 ReactNative.UIManager.configureNextLayoutAnimation = jest.fn();
-ReactNative.InteractionManager.runAfterInteractions = ((cb) => {
+ReactNative.InteractionManager.runAfterInteractions = (cb) => {
   cb && cb();
-});
+};
 ReactNative.Linking.addListener = jest.fn();
 
 const NativeBase = require("native-base");
 NativeBase.Toast.show = jest.fn();
-NativeBase.Root = ({ children }) => (<>{children}</>);
+NativeBase.Root = ({ children }) => <>{children}</>;
 
-import 'react-native-gesture-handler/jestSetup';
-jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
+import "react-native-gesture-handler/jestSetup";
+jest.mock("react-native-reanimated", () => {
+  const Reanimated = require("react-native-reanimated/mock");
 
   // The mock for `call` immediately calls the callback which is incorrect
   // So we override it with a no-op
