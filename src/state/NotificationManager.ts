ddc165fe4a34af99afafff7368a15efbeaca2140
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-06-21 19:42:17 +0400

    WIP

--- src/state/NotificationManager.ts
+++ src/state/NotificationManager.ts
@@ -1,10 +1,14 @@
 import { PermissionsAndroid } from "react-native";
 import { Thunk, thunk } from "easy-peasy";
-import PushNotification, { PushNotificationObject } from "react-native-push-notification";
+// import PushNotification, { PushNotificationObject } from "react-native-push-notification";
 
 import { navigate } from "../utils/navigation";
 import { IStoreModel } from "./index";
-import { ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID, ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_NAME, PLATFORM } from "../utils/constants";
+import {
+  ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
+  ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_NAME,
+  PLATFORM,
+} from "../utils/constants";
 import { localNotification } from "../utils/push-notification";
 import { toast } from "../utils";
 
@@ -13,14 +17,14 @@ const log = logger("NotificationManager");
 
 interface ILocalNotificationPayload {
   message: string;
-  importance?: PushNotificationObject["importance"];
+  // importance?: PushNotificationObject["importance"];
 }
 
 export interface INotificationManagerModel {
   initialize: Thunk<INotificationManagerModel>;
 
-  localNotification: Thunk<INotificationManagerModel, ILocalNotificationPayload,  any, IStoreModel>;
-};
+  localNotification: Thunk<INotificationManagerModel, ILocalNotificationPayload, any, IStoreModel>;
+}
 
 export const notificationManager: INotificationManagerModel = {
   initialize: thunk(async () => {
@@ -35,47 +39,19 @@ export const notificationManager: INotificationManagerModel = {
       if (PLATFORM === "ios") {
         const permissions = await PushNotification.requestPermissions(["alert", "sound", "badge"]);
 
-        if(!permissions.alert) {
+        if (!permissions.alert) {
           log.w("Didn't get permissions to send push notifications.");
           return;
         }
       } else if (PLATFORM === "android") {
         const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
         );
         if (granted === "denied" || granted === "never_ask_again") {
           log.w("Post notification permission was denied", [granted]);
         } else {
           return;
         }
-      }
-
-      PushNotification.configure({
-        requestPermissions: false,
-        onNotification: ((notification) => {
-          log.i("onNotification", [notification]);
-
-          // TODO(hsjoberg): ios notification deeplinking
-          if (PLATFORM === "android") {
-            if (notification.message.toString().includes("on-chain")) {
-              log.i("Navigating to OnChainTransactionLog");
-              navigate("OnChain", { screen: "OnChainTransactionLog" });
-            }
-            else if (notification.message.toString().toLocaleLowerCase().includes("payment channel")) {
-              log.i("Navigating to LightningInfo");
-              navigate("LightningInfo");
-            }
-          }
-        }),
-      });
-
-      if (PLATFORM === "android") {
-        PushNotification.createChannel({
-            channelId: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
-            channelName: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_NAME,
-          },
-          () => {}
-        );
       }
     } catch (error) {
       // TODO(hsjoberg): Perhaps should be handled in the lib instead?
@@ -90,10 +66,7 @@ export const notificationManager: INotificationManagerModel = {
   localNotification: thunk((_, { message, importance }, { getStoreState }) => {
     if (getStoreState().settings.pushNotificationsEnabled) {
       if (PLATFORM !== "macos") {
-        localNotification(
-          message,
-          importance ?? "default"
-        );
+        localNotification(message, importance ?? "default");
       } else {
         toast(message);
       }
--- src/utils/push-notification.ts
+++ src/utils/push-notification.ts
@@ -1,14 +1,7 @@
-import PushNotification, { PushNotificationObject } from "react-native-push-notification";
-import { ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID } from "./constants";
+import { Notifications } from "react-native-notifications";
 
-export const localNotification = (message: string, importance: PushNotificationObject["importance"] = "default"): void => {
-  PushNotification.localNotification({
-    channelId: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
-    message,
-    playSound: true,
-    vibrate: false,
-    priority: "default",
-    importance,
-    autoCancel: true,
+export const localNotification = (message: string): void => {
+  Notifications.postLocalNotification({
+    body: message,
   });
 };
