6cf614c31cd70f27056e08910f42979c344d479b
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-06-23 23:16:38 +0400

    Replace react-native-notifications with notifee

--- src/state/NotificationManager.ts
+++ src/state/NotificationManager.ts
@@ -1,6 +1,10 @@
 import { PermissionsAndroid } from "react-native";
 import { Thunk, thunk } from "easy-peasy";
-import { requestNotifications } from "react-native-permissions";
+import notifee, {
+  AndroidImportance,
+  AndroidVisibility,
+  AuthorizationStatus,
+} from "@notifee/react-native";
 
 import { navigate } from "../utils/navigation";
 import { IStoreModel } from "./index";
@@ -13,13 +17,6 @@ import { localNotification } from "../utils/push-notification";
 import { toast } from "../utils";
 
 import logger from "./../utils/log";
-import {
-  Notification,
-  NotificationBackgroundFetchResult,
-  NotificationCompletion,
-  Notifications,
-} from "react-native-notifications";
-import { NotificationActionResponse } from "react-native-notifications/lib/dist/interfaces/NotificationActionResponse";
 const log = logger("NotificationManager");
 
 interface ILocalNotificationPayload {
@@ -44,55 +41,21 @@ export const notificationManager: INotificationManagerModel = {
 
       if (PLATFORM === "ios" || PLATFORM === "android") {
         log.i("Requesting permissions");
-        const result = await requestNotifications(["alert", "sound", "badge"]);
-        log.d("request notification status", [result.status]);
+        const result = await notifee.requestPermission();
 
-        if (result.status === "granted" && PLATFORM === "android") {
-          Notifications.setNotificationChannel({
-            channelId: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
+        if (
+          result.authorizationStatus === AuthorizationStatus["AUTHORIZED"] &&
+          PLATFORM === "android"
+        ) {
+          const channelId = await notifee.createChannel({
+            id: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
             name: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_NAME,
-            importance: 3,
-            enableLights: true,
-            enableVibration: true,
-            showBadge: true,
+            visibility: AndroidVisibility["PUBLIC"],
+            importance: AndroidImportance["HIGH"],
+            sound: "default",
           });
         }
       }
-
-      Notifications.events().registerNotificationReceivedForeground(
-        (notification: Notification, completion: (response: NotificationCompletion) => void) => {
-          console.log("Notification Received - Foreground", notification.payload);
-
-          // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-          completion({ alert: true, sound: true, badge: false });
-        },
-      );
-
-      Notifications.events().registerNotificationOpened(
-        (
-          notification: Notification,
-          completion: () => void,
-          action: NotificationActionResponse | undefined,
-        ) => {
-          console.log("Notification opened by device user", notification.payload);
-          console.log(
-            `Notification opened with an action identifier: ${action?.identifier} and response text: ${action?.text}`,
-          );
-          completion();
-        },
-      );
-
-      Notifications.events().registerNotificationReceivedBackground(
-        (
-          notification: Notification,
-          completion: (response: NotificationBackgroundFetchResult) => void,
-        ) => {
-          console.log("Notification Received - Background", notification.payload);
-
-          // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
-          completion({ alert: true, sound: true, badge: false });
-        },
-      );
     } catch (error) {
       // TODO(hsjoberg): Perhaps should be handled in the lib instead?
       if (error.domain === "UNErrorDomain") {
--- src/utils/push-notification.ts
+++ src/utils/push-notification.ts
@@ -1,9 +1,12 @@
-import { Notifications } from "react-native-notifications";
+import notifee from "@notifee/react-native";
+
 import { ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID } from "./constants";
 
 export const localNotification = (message: string): void => {
-  Notifications.postLocalNotification({
+  notifee.displayNotification({
     body: message,
-    android_channel_id: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
+    android: {
+      channelId: ANDROID_PUSH_NOTIFICATION_PUSH_CHANNEL_ID,
+    },
   });
 };
