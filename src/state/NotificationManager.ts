8532c7116f43430a7b8c6ea094327c571f7383bc
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-06-23 17:53:43 +0400

    WIP

--- src/state/NotificationManager.ts
+++ src/state/NotificationManager.ts
@@ -1,5 +1,6 @@
 import { PermissionsAndroid } from "react-native";
 import { Thunk, thunk } from "easy-peasy";
+import { requestNotifications } from "react-native-permissions";
 // import PushNotification, { PushNotificationObject } from "react-native-push-notification";
 
 import { navigate } from "../utils/navigation";
@@ -13,6 +14,13 @@ import { localNotification } from "../utils/push-notification";
 import { toast } from "../utils";
 
 import logger from "./../utils/log";
+import {
+  Notification,
+  NotificationBackgroundFetchResult,
+  NotificationCompletion,
+  Notifications,
+} from "react-native-notifications";
+import { NotificationActionResponse } from "react-native-notifications/lib/dist/interfaces/NotificationActionResponse";
 const log = logger("NotificationManager");
 
 interface ILocalNotificationPayload {
@@ -36,23 +44,46 @@ export const notificationManager: INotificationManagerModel = {
         return;
       }
 
-      if (PLATFORM === "ios") {
-        const permissions = await PushNotification.requestPermissions(["alert", "sound", "badge"]);
-
-        if (!permissions.alert) {
-          log.w("Didn't get permissions to send push notifications.");
-          return;
-        }
-      } else if (PLATFORM === "android") {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
-        );
-        if (granted === "denied" || granted === "never_ask_again") {
-          log.w("Post notification permission was denied", [granted]);
-        } else {
-          return;
-        }
+      if (PLATFORM === "ios" || PLATFORM === "android") {
+        log.i("Requesting permissions");
+        const result = await requestNotifications(["alert", "sound", "badge"]);
+        log.d("request notification status", [result.status]);
       }
+
+      Notifications.events().registerNotificationReceivedForeground(
+        (notification: Notification, completion: (response: NotificationCompletion) => void) => {
+          console.log("Notification Received - Foreground", notification.payload);
+
+          // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+          completion({ alert: true, sound: true, badge: false });
+        },
+      );
+
+      Notifications.events().registerNotificationOpened(
+        (
+          notification: Notification,
+          completion: () => void,
+          action: NotificationActionResponse | undefined,
+        ) => {
+          console.log("Notification opened by device user", notification.payload);
+          console.log(
+            `Notification opened with an action identifier: ${action?.identifier} and response text: ${action?.text}`,
+          );
+          completion();
+        },
+      );
+
+      Notifications.events().registerNotificationReceivedBackground(
+        (
+          notification: Notification,
+          completion: (response: NotificationBackgroundFetchResult) => void,
+        ) => {
+          console.log("Notification Received - Background", notification.payload);
+
+          // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+          completion({ alert: true, sound: true, badge: false });
+        },
+      );
     } catch (error) {
       // TODO(hsjoberg): Perhaps should be handled in the lib instead?
       if (error.domain === "UNErrorDomain") {
