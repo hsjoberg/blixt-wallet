cb8e50d83fd45bd9c24be6a8ad893af70aec4e09
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-10-02 02:22:34 +0400

    Update ICloudBackup.ts to use TurboLnd

--- src/state/ICloudBackup.ts
+++ src/state/ICloudBackup.ts
@@ -8,8 +8,9 @@ import { IStoreModel } from "../state";
 import { waitUntilTrue, timeout, toast } from "../utils";
 import { Chain, Debug, Flavor } from "../utils/build";
 import { getItemObject, StorageItem, setItemObject } from "../storage/app";
-import { checkLndStreamErrorResponse } from "../utils/lndmobile";
-import { LndMobileEventEmitter } from "../utils/event-listener";
+
+import { exportAllChannelBackups, subscribeChannelEvents } from "react-native-turbo-lnd";
+import { MultiChanBackup } from "react-native-turbo-lnd/protos/lightning_pb";
 
 import logger from "./../utils/log";
 const log = logger("ICloudBackup");
@@ -19,16 +20,16 @@ export const ICLOUD_BACKUP_KEY = `blixt-wallet-backup-${Chain}${Debug ? "-debug"
 export interface IICloudBackupModel {
   initialize: Thunk<IICloudBackupModel, void, IStoreInjections, IStoreModel>;
 
-  setupChannelUpdateSubscriptions: Thunk<IICloudBackupModel, void, IStoreInjections, IStoreModel>;
-  makeBackup: Thunk<IICloudBackupModel, void, IStoreInjections, IStoreModel>;
-  getBackup: Thunk<IICloudBackupModel, void, IStoreInjections, IStoreModel, Promise<string>>;
+  setupChannelUpdateSubscriptions: Thunk<IICloudBackupModel, void, any, IStoreModel>;
+  makeBackup: Thunk<IICloudBackupModel, void, any, IStoreModel>;
+  getBackup: Thunk<IICloudBackupModel, void, any, IStoreModel, Promise<string>>;
 
   setChannelUpdateSubscriptionStarted: Action<IICloudBackupModel, boolean>;
   setICloudActive: Action<IICloudBackupModel, boolean>;
 
   channelUpdateSubscriptionStarted: boolean;
   iCloudActive: boolean; // Whether iCloud is active on the system
-};
+}
 
 export const iCloudBackup: IICloudBackupModel = {
   initialize: thunk(async (actions, _, { getState, getStoreState, injections }) => {
@@ -48,7 +49,7 @@ export const iCloudBackup: IICloudBackupModel = {
         try {
           let lastICloudBackup = await getItemObject<number>(StorageItem.lastICloudBackup);
           log.d("lastICloudBackup", [lastICloudBackup]);
-          lastICloudBackup = lastICloudBackup - (60 * 60 * 24 * 1000);
+          lastICloudBackup = lastICloudBackup - 60 * 60 * 24 * 1000;
           const currentDate = new Date().getTime();
           const diff = differenceInDays(currentDate, lastICloudBackup);
           if (diff >= 3) {
@@ -67,45 +68,48 @@ export const iCloudBackup: IICloudBackupModel = {
     log.d("Done");
   }),
 
-  setupChannelUpdateSubscriptions: thunk((actions, _2, { getStoreState, injections }) => {
+  setupChannelUpdateSubscriptions: thunk((actions, _2, { getStoreState }) => {
     log.i("Starting channel update subscription for iCloud channel backup");
 
-    LndMobileEventEmitter.addListener("SubscribeChannelEvents", async (e: any) => {
-      try {
-        if (!getStoreState().settings.iCloudBackupEnabled) {
-          return;
-        }
-        log.d("Received SubscribeChannelEvents");
+    subscribeChannelEvents(
+      {},
+      async (channelEvent) => {
+        try {
+          if (!getStoreState().settings.iCloudBackupEnabled) {
+            return;
+          }
+          log.d("Received SubscribeChannelEvents");
 
-        const error = checkLndStreamErrorResponse("SubscribeChannelEvents", e);
-        if (error === "EOF") {
-          return;
-        } else if (error) {
-          throw error
+          if (
+            channelEvent.channel.case === "openChannel" ||
+            channelEvent.channel.case === "closedChannel"
+          ) {
+            log.i("New channel event received, starting new iCloud backup");
+            await actions.makeBackup();
+          }
+        } catch (error: any) {
+          toast("Error backing up to iCloud: " + error.message, undefined, "danger");
         }
+      },
+      (error) => {
+        toast("subscribeChannelEvents: " + error, undefined, "danger");
+      },
+    );
 
-        const decodeChannelEvent = injections.lndMobile.channel.decodeChannelEvent;
-        const channelEvent = decodeChannelEvent(e.data);
-        if (channelEvent.openChannel || channelEvent.closedChannel) {
-          log.i("New channel event received, starting new iCloud backup");
-          await actions.makeBackup();
-        }
-      } catch (error) {
-        toast("Error backing up to iCloud: " + error.message, undefined, "danger");
-      }
-    });
     actions.setChannelUpdateSubscriptionStarted(true);
   }),
 
-  makeBackup: thunk(async (_, _2, { injections }) => {
-    const exportAllChannelBackups = injections.lndMobile.channel.exportAllChannelBackups;
-    const backup = await exportAllChannelBackups();
-    const backupsB64 = base64.fromByteArray(backup.multiChanBackup!.multiChanBackup!);
+  makeBackup: thunk(async (_, _2) => {
+    const backup = await exportAllChannelBackups({});
+    const backupsB64 = base64.fromByteArray(
+      (backup.multiChanBackup as MultiChanBackup).multiChanBackup,
+    ); // TURBOLND uh weird
+
     await iCloudStorage.setItem(ICLOUD_BACKUP_KEY, backupsB64);
 
     const remoteStoredBackupsB64 = await iCloudStorage.getItem(ICLOUD_BACKUP_KEY);
     if (remoteStoredBackupsB64 !== backupsB64) {
-      log.i("iCloud storage mismatch, local, remote:", [backupsB64, remoteStoredBackupsB64])
+      log.i("iCloud storage mismatch, local, remote:", [backupsB64, remoteStoredBackupsB64]);
       throw new Error("Could not save iCloud backup");
     }
 
@@ -116,9 +120,13 @@ export const iCloudBackup: IICloudBackupModel = {
     return await iCloudStorage.getItem(ICLOUD_BACKUP_KEY);
   }),
 
-  setChannelUpdateSubscriptionStarted: action((state, payload) => { state.channelUpdateSubscriptionStarted = payload; }),
-  setICloudActive: action((state, payload) => { state.iCloudActive = payload; }),
+  setChannelUpdateSubscriptionStarted: action((state, payload) => {
+    state.channelUpdateSubscriptionStarted = payload;
+  }),
+  setICloudActive: action((state, payload) => {
+    state.iCloudActive = payload;
+  }),
 
   channelUpdateSubscriptionStarted: false,
   iCloudActive: false,
-}
+};
