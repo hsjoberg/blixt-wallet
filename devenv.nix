a6fac7a9cdd3455ac80ae0bc1bd906897996ac1c
Nitesh Balusu <niteshbalusu@icloud.com>
2024-10-16 15:44:43 -0400

    add support for nix config to the repo

---
+++ devenv.nix
@@ -0,0 +1,22 @@
+{ pkgs, lib, config, inputs, ... }:
+{
+  env.GREET = "devenv";
+  env.YARN_CHECKSUM_BEHAVIOR = "reset";
+  packages = [ pkgs.git ];
+  android = {
+    enable = true;
+    reactNative.enable = true;
+    android-studio.enable = false;
+  };
+  languages.javascript.enable = true;
+  languages.javascript.yarn.enable = true;
+  languages.javascript.corepack.enable = true;
+  languages.typescript.enable = true;
+
+  scripts = {
+    android-init.exec = ''
+      node node_modules/react-native-turbo-lnd/src/fetch-lnd.js
+      echo "sdk.dir=$ANDROID_HOME" > android/local.properties
+    '';
+  };
+}
