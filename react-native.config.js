0b453a36930a57c37847af9174c29c0c4ec9d3ca
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2024-10-30 17:15:05 +0400

    React Native 0.76

--- react-native.config.js
+++ react-native.config.js
@@ -7,11 +7,6 @@ module.exports = {
     macos: {},
   },
   dependencies: {
-    "@react-native-google-signin/google-signin": {
-      platforms: {
-        ios: null,
-      },
-    },
     "react-native-tor": {
       platforms: {
         android: null,
