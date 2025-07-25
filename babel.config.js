c11afb057a3328041a52aa615e1c796fae1aef4a
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2025-07-25 11:42:42 +0200

    Update node modules

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: ["@babel/plugin-transform-export-namespace-from", "react-native-reanimated/plugin"],
+  plugins: ["@babel/plugin-transform-export-namespace-from", "react-native-worklets/plugin"],
 };
