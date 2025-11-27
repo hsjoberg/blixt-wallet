e85cd98ecea10148ee34d570d115c9d7d022e304
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2025-11-27 21:36:05 +0400

    Enable React Compiler 🔥

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
 module.exports = {
   presets: ["module:@react-native/babel-preset"],
-  plugins: ["@babel/plugin-transform-export-namespace-from", "react-native-worklets/plugin"],
+  plugins: [
+    [
+      "babel-plugin-react-compiler",
+      {
+        logger: {
+          logEvent: (filename, event) => {
+            if (event.kind === "CompileSuccess") {
+              console.log("✨ React Compiler successfully compiled: ", filename);
+            }
+            if (event.kind === "CompileError") {
+              console.warn("⚠️ React Compiler failed to compile: " + filename);
+              console.warn(filename, JSON.stringify(event, null, 2));
+            }
+          },
+        },
+      },
+    ],
+    "@babel/plugin-transform-export-namespace-from",
+    "react-native-worklets/plugin",
+  ],
 };
