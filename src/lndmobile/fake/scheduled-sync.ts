46aab28c13216385981be2bee9c9b8dd6c856d62
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2026-02-25 00:56:50 +0400

    Remove src/lndmobile/fake

--- src/lndmobile/fake/scheduled-sync.ts
+++
@@ -1,5 +0,0 @@
-export type WorkInfo = "BLOCKED" | "CANCELLED" | "ENQUEUED" | "FAILED" | "RUNNING" | "SUCCEEDED" | "WORK_NOT_EXIST";
-
-export const checkScheduledSyncWorkStatus = async (): Promise<WorkInfo> => {
-  return "ENQUEUED";
-};
