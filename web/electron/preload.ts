bcc07319072c481920bb6da27f4b1e8690fb62cf
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2026-03-03 22:00:50 +0400

    Remove Electron and Webpack

--- web/electron/preload.ts
+++
@@ -1,9 +0,0 @@
-import "react-native-electron/preload";
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("lndmobile", {
-  hello: () => {
-    ipcRenderer.send("blixt-prompt");
-    return 123;
-  }
-});
