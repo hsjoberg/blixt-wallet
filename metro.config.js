6f0280d83e68e36cd100bf64550ac9afed5cb29a
Hampus Sjöberg <hampus.sjoberg@protonmail.com>
2026-05-08 20:24:04 +0400

    WIP

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,38 @@
+const crypto = require("crypto");
+const path = require("path");
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+
+function createDeterministicModuleIdFactory(projectRoot) {
+  const fileToIdMap = new Map();
+  const usedIds = new Set();
+
+  return (modulePath) => {
+    const cachedId = fileToIdMap.get(modulePath);
+    if (cachedId != null) {
+      return cachedId;
+    }
+
+    const relativePath = path
+      .relative(projectRoot, modulePath)
+      .split(path.sep)
+      .join("/");
+
+    let salt = 0;
+    let id;
+    do {
+      const hash = crypto
+        .createHash("sha1")
+        .update(`${relativePath}:${salt}`)
+        .digest("hex");
+      id = Number.parseInt(hash.slice(0, 8), 16) & 0x7fffffff;
+      salt += 1;
+    } while (usedIds.has(id));
+
+    fileToIdMap.set(modulePath, id);
+    usedIds.add(id);
+    return id;
+  };
+}
 
 /**
  * Metro configuration
@@ -9,6 +43,9 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const config = {
   resolver: {
     unstable_enablePackageExports: true,
+  },
+  serializer: {
+    createModuleIdFactory: () => createDeterministicModuleIdFactory(__dirname),
   },
 };
 
